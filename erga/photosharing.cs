6fff76d971336279223067e7cfb885f75006db94
Tunec <112854325+Tunec6@users.noreply.github.com>
2023-12-15 11:45:54 +0300

    Add files via upload

---
+++ erga/photosharing.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+try{
+    await tcpClient.ConnectAsync("192.168.220.139", 1777);
+    while(true)
+    {
+
+        string path = ".img/123.jpg";
+
+        byte[] reqestData = File.ReadAllBytes(path);
+
+        await tcpClient.SendAsync(reqestData);
+
+        Console.WriteLine("Изображение отправленно!");
+        
+    }
+}
+catch (Exception ex){
+    Console.WriteLine(ex.Message);
+}
+
+
+var a = 1
